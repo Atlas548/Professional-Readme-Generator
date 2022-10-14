a0c7d1f96684777010794d06fcaacbaed2d6df07
Tyler Oates <oatesrotc548@gmail.com>
2022-10-14 13:31:08 -0700

    Update for index.js file

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    message: 'What would you like to name your Project?',
+    name: 'title' 
+},
+{
+    type: 'input',
+    message: 'Describe your application.',
+    name: 'description'
+},
+{
+    type: 'input',
+    message: 'Describe your appliction instructions',
+    name: 'installation'
+},
+{
+    type: 'list',
+    message: 'Please choose the licenses you want to use for your project.',
+    name: 'licenses',
+    choices: [
+        {
+            value: 'BSD3',
+        },
+        {
+            value: 'MIT',
+        },
+    ]
+}
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
