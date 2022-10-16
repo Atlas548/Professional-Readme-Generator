5c82a2312100cad42b1bc0701f368086d158c445
Tyler Oates <oatesrotc548@gmail.com>
2022-10-16 13:38:41 -0700

    went a different route for the renderlicensebadge, see comments under this commit

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
-  if (license !== 'None') {
-    return `![Badge](https:img.shields.io/badge/License-${license}-blue.svg)`
-  } return '';
+  if (license === 'GNU'){
+    return `[![license: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else if (license === 'MIT'){
+    return `[![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+  else if (license === 'ISC'){
+    return `[!license: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license !== 'None') {
-    return `\n* [License](#license)\n`;
-  } return '';
-}
+function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
