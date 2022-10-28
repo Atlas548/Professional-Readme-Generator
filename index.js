2685f6c0df8776cf951cc18358a13548759268c8
Tyler Oates <oatesrotc548@gmail.com>
2022-10-27 20:21:43 -0700

    added questions to be prompt in a arrow function

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { type } = require('os');
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
         {
-            type: ''
+            type: "input",
+            name: "apptitle",
+            message: "Enter the name of your application."
+        },
+        {
+            type: "input",
+            name: "appdescription",
+            message: "Describe your application.",
+        },
+        {
+            type: "input",
+            name: "useremail",
+            message: "Enter your email.",
+        },
+        {
+            type: "input",
+            name: "usergithub",
+            message: "Enter your GitHub username.",
+        },
+        {
+            type: "list",
+            name: "license's",
+            message: "Choose which license your project will have."
+            choices: [
+                { value: "GNU" },
+                { value: "MIT" },
+                { value: "ISC" },
+                { value: "None"},
+            ]
+        },
+        {
+            type: "input",
+            name: "applicationtesting",
+            message: "How do you want to test your application",
+        },
+        {
+            type: "input",
+            name: "usertesting",
+            message: "How & what information does the user need to know in order to use the repo ",
+        },
+        {
+            type: "input",
+            name: "deployapplication",
+            message: "Where to deploy the application",
         }
     ])
 }
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-// TODO: Create a function to initialize app
-function init() {}
-// Function call to initialize app
-init();
+
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+// // TODO: Create a function to initialize app
+// function init() {}
+// // Function call to initialize app
+// init();
