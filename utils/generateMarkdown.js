89784df9a3a4cf18cd96120af78a1e6161a40a90
Tyler Oates <oatesrotc548@gmail.com>
2022-10-27 20:22:20 -0700

    added if else statements for renderlicensebadges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+    if (license === 'GNU'){
+        return `[![license: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    }
+    else if (license === 'MIT'){
+        return `[![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    }
+    else if (license === 'ISC'){
+        return `[!license: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`
+    }
+    else (license === 'None'){
+        return ``;
+    }
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
